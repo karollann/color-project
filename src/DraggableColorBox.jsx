d70043de8d1151dda381937fdf0c7ad2f968581f
karollann <lannkarol@gmail.com>
2022-11-06 20:15:38 +0100

    Add Style DraggableColorBox

--- src/DraggableColorBox.jsx
+++ src/DraggableColorBox.jsx
@@ -1,9 +1,13 @@
 import "./DraggableColorBox.css";
+import DeleteIcon from "@mui/icons-material/Delete";
 
 export const DraggableColorBox = ({ color, name }) => {
   return (
     <div style={{ backgroundColor: color }} className="DraggableColorBox">
-      {color}
+      <div className="DraggableColorBox--content">
+        <span>{name}</span>
+        <DeleteIcon className="DraggableColorBox__deleteIcon" />
+      </div>
     </div>
   );
 };
